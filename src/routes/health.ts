0a2a6b1233a33adfd86a1a4c87fa33229737254e
Quentin Tennerel <tennerelquentin@gmail.com>
2026-03-12 18:42:51 +0800

    feat: add /health to swagger doc

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,16 +1,20 @@
 import { Router } from 'express';
+import { registry } from '../openapi-registry';
 
 const router = Router();
 
-/**
- * @openapi
- * /health:
- * get:
- * summary: Vérifie la santé du BFF
- * responses:
- * 200:
- * description: OK
- */
+registry.registerPath({
+  method: 'get',
+  path: '/health',
+  tags: ['Connectivity'],
+  summary: "Vérifie la santé du BFF",
+  responses: {
+    200: {
+      description: 'OK',
+    }
+  },
+});
+
 router.get('/', (req, res) => {
   res.json({ status: 'ok' });
 });
